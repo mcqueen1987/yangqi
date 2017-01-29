a3112c553c554af268ac9fe6930779151162bc8f
xdb1987 <xdb1987@gmail.com>
2017-01-29 14:46:50 +0800

    just for test

--- crawler/models/index.js
+++ crawler/models/index.js
@@ -4,6 +4,10 @@ var fs = require('fs');
 var path = require('path');
 var db = require('../../utils/dbconfig.js');
 var sequelize = db.sequelize;
+var Sequelize = db.Sequelize;
+/*
+  aaaaaaaaaaaa test
+ */
 
 fs.readdirSync(__dirname)//__dirname,当前文件绝对路径
 	.filter(function(file) {
