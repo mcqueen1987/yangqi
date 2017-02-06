d2df5847dfd4971e1e08ca8acfd683d5c3f3df1f
xdb1987 <xdb1987@gmail.com>
2017-02-06 13:16:37 +0800

    Merge branch 'master' of https://github.com/mcqueen1987/yangqi
    # Conflicts:
    #	common/models/_systemConfig.js

--- crawler/controller/main.js
+++ crawler/controller/main.js
@@ -12,23 +12,8 @@ var cookies = require('../../utils/zhihu.js');
 var Entities = require('html-entities').AllHtmlEntities;
 var entities = new Entities();
 var controller = {};
-
-// var CZUser = require('../servlet/CZUser.js');
-// var CZQuizFollow = require('../servlet/CZQuizFollow.js');
-// var CZQuiz = require('../servlet/CZQuiz.js');
-// var CZQuizTags = require('../servlet/CZQuizTags.js');
 var mytable = require('../servlet/mytable.js');
-// var CZList = require('../servlet/CZList.js');
 var _systemConfig = require('../../common/servlet/_systemConfig.js').servlet;
-
-
-// //查询出全部未删除的问题
-// function queryAllQuiz(req, res, next) {
-// 	CZQuiz.CZQuiz.findAll().then(function (result) {
-// 		res.render('list', { result: result });
-// 	});
-// }
-// controller.queryAllQuiz = queryAllQuiz;
 
 
 //根据问题查询出该问题以及该问题的答案
@@ -91,80 +76,11 @@ controller.saveMyTable = saveMyTable;
 var crawlerPeopleAbout = function (result, uri) {
     var cZUser = {};
     var $ = cheerio.load(result.text);
-    // var rule = /bdServerTime(\S*)<\/script/;
-    // var uid = JSON.parse(regexStr(result.text, rule));
-    // cZUser.uid = uid[0];
-    // cZUser.ucode = uid[1];
     cZUser.href = uri;
     cZUser.title = $('title').text();
-    // cZUser.avatar = $('.zm-profile-header-main .body img').attr('src');
-    // cZUser.brief = $('.title-section.ellipsis span').text();
-    // var describe = $('.zm-profile-header-main .body  .info-wrap .description span').html();
-    // if (describe && describe.length > 0) cZUser.describe = entities.decode(describe);
-    // if ($('.gender .icon-profile-female').length == 1) {
-    //     cZUser.sex = '女';
-    // } else if ($('.gender .icon-profile-male').length == 1) {
-    //     cZUser.sex = '男';
-    // } else {
-    //     cZUser.sex = '未知';
-    // }
-    // var followees = $('.zu-main-sidebar .zm-profile-side-following .item:nth-child(1)').children('strong').text();
-    // cZUser.followees = followees;
-    // var followers = $('.zu-main-sidebar .zm-profile-side-following .item:nth-child(2)').children('strong').text();
-    // cZUser.followers = followers;
-    // var columns = $('.zm-profile-side-columns').prev('.zm-profile-side-section-title').children('a').children('strong').html();
-    // if (columns) {
-    //     cZUser.columns = columns;
-    // }
-    // var topics = $('.zm-profile-side-topics').prev('.zm-profile-side-section-title').children('a').children('strong').html();
-    // if (topics) {
-    //     cZUser.topics = (topics + '').replace(/[^0-9]+/ig, "");
-    // }
-    // cZUser.agree = $('.zm-profile-details-reputation .zm-profile-module-desc span:nth-child(3)').children('strong').text();
-    // cZUser.thanks = $('.zm-profile-details-reputation .zm-profile-module-desc span:nth-child(5)').children('strong').text();
-    // cZUser.asks = $('.profile-navbar a:nth-child(2) .num').text();
-    // cZUser.answers = $('.profile-navbar a:nth-child(3) .num').text();
-    // cZUser.posts = $('.profile-navbar a:nth-child(4) .num').text();
-    // cZUser.view = $('div.zg-wrap.zu-main.clearfix > div.zu-main-sidebar .zm-profile-side-section span.zg-gray-normal strong').text();
-    // var weibo = $('.weibo-wrap a').attr('href');
-    // if (weibo) cZUser.weibo = weibo;
-    // cZUser.location = $('div.zu-main-content > div > div.zm-profile-header.ProfileCard > div.zm-profile-header-main > div.body.clearfix > div > div > div > div:nth-child(1) > span.info-wrap > span.location.item a').text();
-    // cZUser.business = $('div.zu-main-content > div > div.zm-profile-header.ProfileCard > div.zm-profile-header-main > div.body.clearfix > div > div > div > div:nth-child(1) > span.info-wrap > span.business.item > a').text();
-    // cZUser.employment = $('div.zu-main-content > div > div.zm-profile-header.ProfileCard > div.zm-profile-header-main > div.body.clearfix > div > div > div.items > div:nth-child(2) > span.info-wrap > span.employment.item').text();
-    // cZUser.positions = $('div.zm-profile-header.ProfileCard > div.zm-profile-header-main > div.body.clearfix > div > div > div.items > div:nth-child(2) > span.info-wrap > span.position.item > a').text();
-    // var education = $('body > div.zg-wrap.zu-main.clearfix > div.zu-main-content > div > div.ProfileCard.zm-profile-details-wrap > div.zm-profile-section-list.zm-profile-details > div:nth-child(4) > div > div').html();
-    // if (education) cZUser.education = education;
-    // cZUser.educationExtra = $('body > div.zg-wrap.zu-main.clearfix > div.zu-main-content > div > div.ProfileCard.zm-profile-details-wrap > div.zm-profile-section-list.zm-profile-details > div:nth-child(4) > div > div > div > div ').children().eq(2).text();
-    // cZUser.collect = $('.zm-profile-details-reputation .zm-profile-module-desc span:nth-child(7)').children('strong').text().replace(/[^0-9]+/ig, "");
-    // cZUser.share = $('.zm-profile-details-reputation .zm-profile-module-desc span:nth-child(9)').children('strong').text();
-    // var marked = $('.zm-profile-header-marked .zg-link-litblue-normal').text();
-    // cZUser.marked = marked;
     return cZUser;
 }
 
-/**
- * 查询list表中符合条件的数据放入数组中,循环处理
- */
-function getPeopleInfo() {
-    console.log('-----------  getPeopleInfo function ---------------');
-    var configMap = _systemConfig.configMap;
-    console.log('----------- in  getpeople info function ---------------' + JSON.stringify(configMap));
-    async.series([
-        setTimeout(function () {
-            console.log('------getPeopleInfo  --------async.eachSeries ------');
-            peopleInfo(function(msg){
-                if(msg === 'success'){
-                    console.log('===============get parse data success===============');
-                }else{
-                    console.log('===============get parse data fail===============');
-                }
-            });
-        }, 3000),
-    ], function(err) { //This is the final callback
-        console.log('oops,出错了!!!' + err);
-        logger.error('oops,出错了!!!' + err);
-    });
-}
 
 /**
  * 传入用户的个人中心详细页面url,截取内容并且保存到数据库中
@@ -194,8 +110,8 @@ function peopleInfo(callback) {
                     var parsedData = crawlerPeopleAbout(result, uri);
                     logger.info('用户个人中心地址  line 198 ' + + JSON.stringify(parsedData));
                     saveMyTable({
-                        firstName : '222222',
-                        lastName : '555555555'
+                        firstName : '22227777777722',
+                        lastName : '555577777777755555'
                     });
                     callback("success");
                 } catch (e) {
@@ -208,28 +124,29 @@ function peopleInfo(callback) {
         });
 }
 
-
 /**
- * 正则截取数据
- * @param str   待截取的字符串
- * @param rule  正则表达式
- * @returns {*} 如果成功,则返回字符串,如果失败则为''
+ * 查询list表中符合条件的数据放入数组中,循环处理
  */
-function regexStr(str, rule) {
-    var subStr = str.match(rule);
-    try {
-        if (subStr) {
-            return subStr[1];
-        } else {
-            return '';
-        }
-    } catch (e) {
-        logger.info('字符串:' + str + ' 正则' + rule + '正则截取报错:' + e);
-        return '';
-    }
+function getPeopleInfo() {
+    console.log('-----------  getPeopleInfo function ---------------');
+    var configMap = _systemConfig.configMap;
+    console.log('----------- in  getpeople info function ---------------' + JSON.stringify(configMap));
+    async.series([
+        setTimeout(function () {
+            console.log('------getPeopleInfo  --------async.eachSeries ------');
+            peopleInfo(function(msg){
+                if(msg === 'success'){
+                    console.log('===============get parse data success===============');
+                }else{
+                    console.log('===============get parse data fail===============');
+                }
+            });
+        }, 3000),
+    ], function(err) { //This is the final callback
+        console.log('oops,出错了!!!' + err);
+        logger.error('oops,出错了!!!' + err);
+    });
 }
-// module.exports.peopleInfo = peopleInfo;
-// module.exports.getPeopleInfo = getPeopleInfo;
 
 controller.peopleInfo = peopleInfo;
 controller.getPeopleInfo = getPeopleInfo;
